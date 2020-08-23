0cad6d6e6105c0c0028aa864d9a3893103862d7f
Åke Engelbrektson <eson57@users.noreply.github.com>
2020-08-23 07:52:18 +0200

    Create harbour-sfos-forum-viewer-sv.ts
    Add Swedish translation

---
+++ translations/harbour-sfos-forum-viewer-sv.ts
@@ -0,0 +1,208 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>SFOS Forum Viewer for Sailfish OS v%1
+By szopin
+Licensed under MIT
+
+App icon by dseight
+AboutPage art by Hanibu
+
+Special thanks to contributors:
+</source>
+        <translation>SFOS Forum-visare för Sailfish OS v%1
+Av szopin
+Licensierad under MIT
+
+App-ikon av dseight
+Om-sidekonst av Hanibu
+
+Speciellt tack till medverkande:
+</translation>
+    </message>
+    <message>
+        <source>, </source>
+        <extracomment>contributors list separator</extracomment>
+        <translation>, </translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>Källkod</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+</context>
+<context>
+    <name>CategorySelect</name>
+    <message>
+        <source>Categories</source>
+        <translation>Kategorier</translation>
+    </message>
+    <message>
+        <source>Latest</source>
+        <translation>Senast</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Topp</translation>
+    </message>
+    <message>
+        <source>Nothing to show</source>
+        <translation>Inget att visa</translation>
+    </message>
+    <message>
+        <source>Is the network enabled?</source>
+        <translation>Är nätverket aktiverat?</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Latest posts</source>
+        <translation>Senaste inläggen</translation>
+    </message>
+    <message>
+        <source>SailfishOS</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>SailfishOS</translation>
+    </message>
+    <message>
+        <source>Forum Viewer</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>Forum-visare</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Latest</source>
+        <translation>Senast</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Topp</translation>
+    </message>
+    <message>
+        <source>SailfishOS Forum</source>
+        <translation>SailfishOS Forum</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Hämta om</translation>
+    </message>
+    <message>
+        <source>Nothing to show</source>
+        <translation>Inget att visa</translation>
+    </message>
+    <message>
+        <source>Is the network enabled?</source>
+        <translation>Är nätverket aktiverat?</translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation>Hämta mer</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+</context>
+<context>
+    <name>OpenLink</name>
+    <message>
+        <source>Open URL in webview?</source>
+        <translation>Vill du öppna URL i webbvy?</translation>
+    </message>
+    <message>
+        <source>Open externally</source>
+        <translation>Öppna externt</translation>
+    </message>
+</context>
+<context>
+    <name>PostView</name>
+    <message>
+        <source>Revision history</source>
+        <translation>Revisionshistorik</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <source>Searching in “%1”</source>
+        <translation>Söker i &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <source>No results</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>Inga träffar</translation>
+    </message>
+    <message>
+        <source>in “%1”</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>i &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <source>Search in all threads</source>
+        <translation>Sök i alla trådar</translation>
+    </message>
+    <message>
+        <source>Search in the current thread</source>
+        <translation>Sök i aktuell tråd</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+</context>
+<context>
+    <name>ThreadView</name>
+    <message>
+        <source>Open in external browser</source>
+        <translation>Öppna i extern webbläsare</translation>
+    </message>
+    <message>
+        <source>Open directly</source>
+        <translation>Öppna direkt</translation>
+    </message>
+    <message>
+        <source>Search thread</source>
+        <translation>Sök tråd</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n like(s)</source>
+        <translation>
+            <numerusform>%n like</numerusform>
+            <numerusform>%n likes</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>✍️: %1</source>
+        <translation>✍️: %1</translation>
+    </message>
+    <message>
+        <source>Revision history</source>
+        <translation>Revisionshistorik</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-sfos-forum-viewer</name>
+    <message>
+        <source>d/M/yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <extracomment>date format including date and time but no weekday</extracomment>
+        <translation>yyyy-M-d (hh:mm)</translation>
+    </message>
+</context>
+</TS>
