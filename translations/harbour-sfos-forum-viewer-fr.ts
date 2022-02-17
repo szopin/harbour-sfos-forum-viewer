3931d75598635421673c02d8b25ae069998e953a
Patrick Hervieux <4703153-pherjung@users.noreply.gitlab.com>
2022-02-17 18:22:18 +0100

    Add French translation

---
+++ translations/harbour-sfos-forum-viewer-fr.ts
@@ -0,0 +1,310 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>SFOS Forum Viewer for Sailfish OS v%1
+By szopin
+Licensed under MIT
+
+App icon by dseight
+AboutPage art by Hanibu
+
+Special thanks to contributors:
+</source>
+        <translation>Visionneur du forum SFOS pour Sailfish OS : v%1
+Par szopin
+Licence MIT
+
+Design de l&apos;icone par dseight
+AboutPage art par Hanibu
+
+Remerciements aux contributeurs :
+</translation>
+    </message>
+    <message>
+        <source>, </source>
+        <extracomment>contributors list separator</extracomment>
+        <translation>, </translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>Sources</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licence</translation>
+    </message>
+</context>
+<context>
+    <name>CategorySelect</name>
+    <message>
+        <source>Categories</source>
+        <translation>Catégories</translation>
+    </message>
+    <message>
+        <source>Latest</source>
+        <translation>Dernier</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Top</translation>
+    </message>
+    <message>
+        <source>Nothing to show</source>
+        <translation>Rien à montrer</translation>
+    </message>
+    <message>
+        <source>Is the network enabled?</source>
+        <translation>Le réseau est-il activé ?</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Latest posts</source>
+        <translation>Derniers commentaires</translation>
+    </message>
+    <message>
+        <source>SailfishOS</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>SailfishOS</translation>
+    </message>
+    <message>
+        <source>Forum Viewer</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>Visionneur de forum</translation>
+    </message>
+</context>
+<context>
+    <name>Error</name>
+    <message>
+        <source>Error:</source>
+        <translation>Erreur :</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Latest</source>
+        <translation>Dernier</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Top</translation>
+    </message>
+    <message>
+        <source>SailfishOS Forum</source>
+        <translation>SailfishOS Forum</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Recharger</translation>
+    </message>
+    <message>
+        <source>Nothing to show</source>
+        <translation>Rien à montrer</translation>
+    </message>
+    <message>
+        <source>Is the network enabled?</source>
+        <translation>Le réseau est-il activé ?</translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation>Charger plus</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Rechercher</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Connexion</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Déconnexion</translation>
+    </message>
+    <message>
+        <source>New thread</source>
+        <translation>Nouveau fil de discussion</translation>
+    </message>
+</context>
+<context>
+    <name>NewPost</name>
+    <message>
+        <source>Enter post</source>
+        <translation>Insérer un commentaire</translation>
+    </message>
+    <message>
+        <source>Body</source>
+        <translation>Corps</translation>
+    </message>
+    <message>
+        <source>Edit post</source>
+        <translation>Éditer le commentaire</translation>
+    </message>
+    <message>
+        <source>Insert quote</source>
+        <translation>Insérer une citation</translation>
+    </message>
+</context>
+<context>
+    <name>NewThread</name>
+    <message>
+        <source>Enter thread</source>
+        <translation>Entrer dans le fil de discussion</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Titre</translation>
+    </message>
+    <message>
+        <source>Body</source>
+        <translation>Corps</translation>
+    </message>
+</context>
+<context>
+    <name>OpenLink</name>
+    <message>
+        <source>Open URL in webview?</source>
+        <translation>Ouvrir l&apos;URL dans le WebView ?</translation>
+    </message>
+    <message>
+        <source>Open externally</source>
+        <translation>Ouvrir le lien avec</translation>
+    </message>
+</context>
+<context>
+    <name>PostView</name>
+    <message>
+        <source>Revision history</source>
+        <translation>Révision de l&apos;historique</translation>
+    </message>
+    <message>
+        <source>Previous</source>
+        <translation>Précédent</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Suivant</translation>
+    </message>
+    <message>
+        <source>Alternative formatting</source>
+        <translation>Formattage alternatif</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <source>Searching in “%1”</source>
+        <translation>Recherche dans “%1”</translation>
+    </message>
+    <message>
+        <source>No results</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>Aucun résultats</translation>
+    </message>
+    <message>
+        <source>in “%1”</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>dans “%1”</translation>
+    </message>
+    <message>
+        <source>Search in all threads</source>
+        <translation>Rechercher dans tous les fils de discussion</translation>
+    </message>
+    <message>
+        <source>Search in the current thread</source>
+        <translation>Rechercher dans le fil actuel</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Rechercher</translation>
+    </message>
+</context>
+<context>
+    <name>ThreadView</name>
+    <message>
+        <source>Copy link to clipboard</source>
+        <translation>Copier le lien</translation>
+    </message>
+    <message>
+        <source>Open in external browser</source>
+        <translation>Ouvrir dans le navigateur</translation>
+    </message>
+    <message>
+        <source>Open directly</source>
+        <translation>Ouvrir directement</translation>
+    </message>
+    <message>
+        <source>Search thread</source>
+        <translation>Rechercher le fil de discussion</translation>
+    </message>
+    <message>
+        <source>Post reply</source>
+        <translation>Publier le commentaire</translation>
+    </message>
+    <message>
+        <source>✍️: %1</source>
+        <translation>✍️: %1</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>Copier</translation>
+    </message>
+    <message>
+        <source>Revision history</source>
+        <translation>Historique</translation>
+    </message>
+    <message>
+        <source>Alternative formatting</source>
+        <translation>Formattage alternatif</translation>
+    </message>
+    <message>
+        <source>Show replied to post</source>
+        <translation>Afficher le message répondu</translation>
+    </message>
+    <message>
+        <source>Uncensor post</source>
+        <translation>Afficher le commentaire</translation>
+    </message>
+    <message>
+        <source>Like</source>
+        <translation>Aimer</translation>
+    </message>
+    <message>
+        <source>Reply</source>
+        <translation>Répondre</translation>
+    </message>
+    <message>
+        <source>Unlike</source>
+        <translation>Ne plus aimer</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Éditer</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-sfos-forum-viewer</name>
+    <message>
+        <source>d/M/yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <extracomment>date format including date and time but no weekday</extracomment>
+        <translation>d/M/yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</translation>
+    </message>
+</context>
+</TS>
