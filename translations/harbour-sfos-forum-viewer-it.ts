67306b32d9fdd7fc142de42880332647bebca33a
legacychimera247 <mad_fer_it@vivaldi.net>
2025-09-21 02:41:15 +0200

    Add files via upload

---
+++ translations/harbour-sfos-forum-viewer-it.ts
@@ -0,0 +1,550 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>A proposito di</translation>
+    </message>
+    <message>
+        <source>, </source>
+        <extracomment>contributors list separator</extracomment>
+        <translation>, </translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>Codice Sorgente</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>Licenza</translation>
+    </message>
+    <message>
+        <source>SFOS Forum Viewer for Sailfish OS v%1
+By szopin
+Licensed under MIT
+
+App icon by dseight
+AboutPage art by Hanibu
+
+Special thanks to contributors:
+</source>
+        <translation>Visualizzatore SFOS Forum per Sailfish OS v%1
+By szopin
+Sotto licenza MIT
+
+Icona app progettata da dseight
+Immagine pagina informazioni fatta da Hanibu
+
+Un grazie speciale ai contributori:
+</translation>
+    </message>
+</context>
+<context>
+    <name>CategorySelect</name>
+    <message>
+        <source>Categories</source>
+        <translation>Categorie</translation>
+    </message>
+    <message>
+        <source>Latest</source>
+        <translation>Recenti</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Più importanti</translation>
+    </message>
+    <message>
+        <source>Nothing to show</source>
+        <translation>Niente da mostrare</translation>
+    </message>
+    <message>
+        <source>Is the network enabled?</source>
+        <translation>La rete è abilitata?</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Latest posts</source>
+        <translation>Ultimi post</translation>
+    </message>
+    <message>
+        <source>SailfishOS</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>SailfishOS</translation>
+    </message>
+    <message>
+        <source>Forum Viewer</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>Forum Viewer</translation>
+    </message>
+</context>
+<context>
+    <name>Error</name>
+    <message>
+        <source>Error:</source>
+        <translation>Errore:</translation>
+    </message>
+    <message>
+        <source>An unknown Error occurred.</source>
+        <translation>Si è veriuficato un errore sconosciuto.</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Latest</source>
+        <translation>Recenti</translation>
+    </message>
+    <message>
+        <source>Top</source>
+        <translation>Più importanti</translation>
+    </message>
+    <message>
+        <source>SailfishOS Forum</source>
+        <translation>Forum di SailfishOS</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>A proposito di</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Effettua l&apos;accesso</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <source>New thread</source>
+        <translation>Nuova discussione</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Cerca</translation>
+    </message>
+    <message>
+        <source>Notifications</source>
+        <translation>Notifiche</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message>
+        <source>Nothing to show</source>
+        <translation>Niente da mostrare</translation>
+    </message>
+    <message>
+        <source>Is the network enabled?</source>
+        <translation>La rete è abilitata?</translation>
+    </message>
+    <message>
+        <source>Load more</source>
+        <translation>Carica altro</translation>
+    </message>
+    <message>
+        <source>New notification</source>
+        <translation>Nuova notifica</translation>
+    </message>
+    <message>
+        <source>tags</source>
+        <translation>tags</translation>
+    </message>
+    <message>
+        <source>Mark as read</source>
+        <translation>Segna come letto</translation>
+    </message>
+    <message>
+        <source>Clear filter list</source>
+        <translation>Pulisci lista filtri</translation>
+    </message>
+    <message>
+        <source>Filter OP</source>
+        <translation>Filtra OP</translation>
+    </message>
+    <message>
+        <source>Muted</source>
+        <comment>Topic watch level (state)</comment>
+        <translation>Silenziato</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <comment>Topic watch action (verb)</comment>
+        <translation>Silenzia</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <comment>Topic watch level (state)</comment>
+        <translation>Normale</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <comment>Topic watch action (verb)</comment>
+        <translation>Normalizza</translation>
+    </message>
+    <message>
+        <source>Tracking</source>
+        <comment>Topic watch level (state)</comment>
+        <translation>Tracciamento</translation>
+    </message>
+    <message>
+        <source>Track</source>
+        <comment>Topic watch action (verb)</comment>
+        <translation>Traccia</translation>
+    </message>
+    <message>
+        <source>Watching</source>
+        <comment>Topic watch level (state)</comment>
+        <translation>Osservando</translation>
+    </message>
+    <message>
+        <source>Watch</source>
+        <comment>Topic watch action (verb)</comment>
+        <translation>Osserva</translation>
+    </message>
+    <message>
+        <source>Don&apos;t track (local)</source>
+        <translation>Non tracciare (locale)</translation>
+    </message>
+    <message>
+        <source>Logging out</source>
+        <translation>Disconnessione</translation>
+    </message>
+    <message>
+        <source>Filtering user</source>
+        <translation>Filtrando utente</translation>
+    </message>
+</context>
+<context>
+    <name>NewPost</name>
+    <message>
+        <source>Enter post</source>
+        <translation>Immetti post</translation>
+    </message>
+    <message>
+        <source>Body</source>
+        <translation>Corpo</translation>
+    </message>
+    <message>
+        <source>Edit post</source>
+        <translation>Modifica post</translation>
+    </message>
+    <message>
+        <source>Insert quote</source>
+        <translation>Inserisci citazione</translation>
+    </message>
+    <message>
+        <source>Upload image (through ImgBB)</source>
+        <translation>Carica immagine (con Imgbb)</translation>
+    </message>
+</context>
+<context>
+    <name>NewThread</name>
+    <message>
+        <source>Enter thread</source>
+        <translation>Inserisci discussione</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Titolo</translation>
+    </message>
+    <message>
+        <source>Body</source>
+        <translation>Corpo</translation>
+    </message>
+    <message>
+        <source>Enter PM to </source>
+        <translation>Invia Messaggio Privato a </translation>
+    </message>
+</context>
+<context>
+    <name>NotificationSettings</name>
+    <message>
+        <source>Accept</source>
+        <translation>Accetta</translation>
+    </message>
+    <message>
+        <source>Check automatically</source>
+        <translation>Controlla automaticamente</translation>
+    </message>
+    <message>
+        <source>Minutes: </source>
+        <translation>Minuti: </translation>
+    </message>
+</context>
+<context>
+    <name>Notifications</name>
+    <message>
+        <source>Notifications</source>
+        <translation>Notifiche</translation>
+    </message>
+    <message>
+        <source>SailfishOS Forum</source>
+        <translation>Forum SailfishOS</translation>
+    </message>
+    <message>
+        <source>Nothing to show</source>
+        <translation>Niente da mostrare</translation>
+    </message>
+    <message>
+        <source>Is the network enabled?</source>
+        <translation>La rete è abilitata?</translation>
+    </message>
+    <message>
+        <source>PMs</source>
+        <translation>Messagi Privati</translation>
+    </message>
+    <message>
+        <source>PMs - sent</source>
+        <translation>Messaggi Privati Inviati</translation>
+    </message>
+    <message>
+        <source>Muted topics</source>
+        <translation>Discussioni silenziate</translation>
+    </message>
+    <message>
+        <source>Unmute</source>
+        <translation>Riattiva notifiche</translation>
+    </message>
+</context>
+<context>
+    <name>OpenLink</name>
+    <message>
+        <source>Open URL in webview?</source>
+        <translation>Aprire URL nella webview?</translation>
+    </message>
+    <message>
+        <source>Open externally</source>
+        <translation>Apri con browser</translation>
+    </message>
+</context>
+<context>
+    <name>PollView</name>
+    <message>
+        <source>Single Answer Poll</source>
+        <translation>sondaggio a Risposta Singola</translation>
+    </message>
+    <message>
+        <source>Multiple Answer Poll</source>
+        <translation>Sondaggio a Risposta Multipla</translation>
+    </message>
+    <message>
+        <source>Rating Poll</source>
+        <translation>Sondaggio con percentuale</translation>
+    </message>
+    <message>
+        <source>Unsupported Poll</source>
+        <translation>Sondaggio non supportato</translation>
+    </message>
+    <message>
+        <source>This poll has more than %1 options, this is not supported.</source>
+        <translation>Questo sondaggio ha più di %1 scelte, questo non è supportato.</translation>
+    </message>
+    <message>
+        <source>Please go back and open the poll in a browser window.</source>
+        <translation>Per favore, torna indietro ed apri il sondaggio in una finestra del browser.</translation>
+    </message>
+    <message>
+        <source>This type of poll is not yet supported: %1</source>
+        <translation>Questo tipo di sondaggio non è ancora supportato: %1</translation>
+    </message>
+    <message>
+        <source>Voters: %1 Status: %2</source>
+        <translation>Votanti: %1 Stato: %2</translation>
+    </message>
+    <message>
+        <source>submitted</source>
+        <translation>voto inviato</translation>
+    </message>
+    <message>
+        <source>Votes: %1</source>
+        <translation>Voti: %1</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Ripristina</translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation>Invia</translation>
+    </message>
+    <message>
+        <source>View Results</source>
+        <translation>Vedi Risultati</translation>
+    </message>
+    <message>
+        <source>Vote</source>
+        <translation>Vota</translation>
+    </message>
+    <message>
+        <source>View Votes</source>
+        <translation>Vedi Voti</translation>
+    </message>
+    <message>
+        <source>not logged in</source>
+        <translation>non hai effettuato l&apos;accesso</translation>
+    </message>
+</context>
+<context>
+    <name>PostView</name>
+    <message>
+        <source>Revision history</source>
+        <translation>Cronologia</translation>
+    </message>
+    <message>
+        <source>Previous</source>
+        <translation>Precedente</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Prossimo</translation>
+    </message>
+    <message>
+        <source>Alternative formatting</source>
+        <translation>Formattazione alternativa</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <source>Searching in “%1”</source>
+        <translation>Sto cercando in “%1”</translation>
+    </message>
+    <message>
+        <source>No results</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>Nessun risultato</translation>
+    </message>
+    <message>
+        <source>in “%1”</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>su “%1”</translation>
+    </message>
+    <message>
+        <source>Search in all threads</source>
+        <translation>Cerca in tutte le discussioni</translation>
+    </message>
+    <message>
+        <source>Search in the current thread</source>
+        <translation>Cerca nella discussione corrente</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Cerca</translation>
+    </message>
+</context>
+<context>
+    <name>ThreadView</name>
+    <message>
+        <source>Copy link to clipboard</source>
+        <translation>Copia collegamento negli appunti</translation>
+    </message>
+    <message>
+        <source>Open in external browser</source>
+        <translation>Apri nel browser esterno</translation>
+    </message>
+    <message>
+        <source>Open directly</source>
+        <translation>Apri direttamente</translation>
+    </message>
+    <message>
+        <source>Search thread</source>
+        <translation>Cerca argomento</translation>
+    </message>
+    <message>
+        <source>Post reply</source>
+        <translation>Invia risposta</translation>
+    </message>
+    <message>
+        <source>✍️: %1</source>
+        <translation>✍️: %1</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>Copia negli appunti</translation>
+    </message>
+    <message>
+        <source>Revision history</source>
+        <translation>Cronologia</translation>
+    </message>
+    <message>
+        <source>Alternative formatting</source>
+        <translation>Formattazione alternativa</translation>
+    </message>
+    <message>
+        <source>Like</source>
+        <translation>Mi piace</translation>
+    </message>
+    <message>
+        <source>Reply</source>
+        <translation>Rispondi</translation>
+    </message>
+    <message>
+        <source>Unlike</source>
+        <translation>Non mi piace più</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Cancella</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Modifica</translation>
+    </message>
+    <message>
+        <source>tags</source>
+        <translation>tags</translation>
+    </message>
+    <message>
+        <source>Filter user</source>
+        <translation>Filtra utente</translation>
+    </message>
+    <message>
+        <source>This post contains polls.</source>
+        <translation>Questo post contiene dei sondaggi.</translation>
+    </message>
+    <message>
+        <source>This post contains a poll.</source>
+        <translation>Questo post contiene un sondaggio.</translation>
+    </message>
+    <message>
+        <source>Click to view and vote:</source>
+        <translation>Clicca per vedere e votare:</translation>
+    </message>
+    <message>
+        <source>Poll</source>
+        <translation>Sondaggio</translation>
+    </message>
+    <message>
+        <source>Filtering user</source>
+        <translation>Filtrando utente</translation>
+    </message>
+    <message>
+        <source>Back to full thread</source>
+        <translation>Torna alla discussione completa</translation>
+    </message>
+    <message>
+        <source>Show replied to post(s)</source>
+        <translation>Mostra risposta ai messaggi</translation>
+    </message>
+</context>
+<context>
+    <name>UserCard</name>
+    <message>
+        <source>PM</source>
+        <translation>Messaggi Privati</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-sfos-forum-viewer</name>
+    <message>
+        <source>d/M/yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <extracomment>date format including date and time but no weekday</extracomment>
+        <translation>d/M/yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</translation>
+    </message>
+</context>
+</TS>
