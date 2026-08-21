szopin/harbour-sfos-forum-viewer

commit ab0d6dd288a959115202d9f24b5f9cb9472de97b

Fix: template-less categories

qml/pages/FirstPage.qml
@@ -458,7 +458,7 @@ Page {
             }
             MenuItem {
                 text: qsTr("New thread")
-                visible: !remorseActive && loggedin.value != "-1" && tid ? true : false
+                visible: !remorseActive && loggedin.value != "-1" && tid && topic_template != "NO-POSTS-ALLOWED" ? true : false
                 onClicked: pageStack.push("NewThread.qml", {category: category, raw: topic_template});
             }
             MenuItem {

qml/harbour-sfos-forum-viewer.qml
@@ -91,7 +91,7 @@ ApplicationWindow
                             topic_count: item['topic_count'],
                             description_text: item['description_text'],
                             slug: item['slug'],
-                            topic_template: item['topic_template'],
+                            topic_template: item['topic_template'] || "NO-POSTS-ALLOWED", // need to discern null(no posts allowed) and ""
                             is_subcategory: (!!isSub),
                             parent_category_id: isSub ? item['parent_category_id'] : -1
                         };

commit cd7a33db282b079e9e763f46e654133a0a0caa7e

Fix 'new thread' for non-read-only categories
ab0d6dd broke the new thread feature.
hopefully this variant is better.

qml/pages/CategorySelect.qml
@@ -211,7 +211,9 @@ Page {
                 */
            }
            onClicked: {
-               findFirstPage().showCategory( ((is_subcategory) ? categories.lookup[parent_category_id].slug + "/" : "") + slug + "/" + topic, name, topic_template, topic);
+                   findFirstPage().showCategory( ((is_subcategory)
+                       ? categories.lookup[parent_category_id].slug + "/" : "")
+                       + slug + "/" + topic, name, topic_template, topic);
                pageStack.navigateBack();
            }
            Rectangle {

qml/pages/FirstPage.qml
@@ -37,6 +37,7 @@ Page {
     property string tid
     property string category
     property string topic_template
+    property bool new_topic_allowed
     property int pageno: 0
     property int timerv
     property string lastnotv
@@ -311,7 +312,8 @@ Page {
         viewmode = "";
         tid = showTopic;
         textname = showTextname;
-        topic_template = template;
+        topic_template = !!template ? template : "";
+        new_topic_allowed = (typeof template !== "undefined") // if disallowed this will be null in JSON, -> undefined in qml
         category = cat;
         clearview();
 
@@ -486,7 +488,7 @@ Page {
             }
             MenuItem {
                 text: qsTr("New thread")
-                visible: !remorseActive && loggedin.value != "-1" && tid ? true : false
+                visible: !remorseActive && loggedin.value != "-1" && tid && new_topic_allowed ? true : false
                 onClicked: pageStack.push("NewThread.qml", {category: category, raw: topic_template});
             }
             MenuItem {
@@ -517,6 +519,10 @@ Page {
                     application.fetchLatestPosts()
 
                 }
+            }
+            MenuLabel {
+                visible: !new_topic_allowed && loggedin.value != "-1"
+                text: qsTr("You cannot create threads this categury")
             }
         }
 